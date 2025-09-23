f2a19affed2dff04a49cfb39aa19bc2e590862df
merimatikvesa <merima_tikvesa@hotmail.com>
2025-09-23 15:32:20 +0200

    #62 Send JWT via HttpInterceptor; add AuthGuard andpublic ?products route and private ?orders route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,48 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
+  //public
+  {
+    path: 'products',
+    loadComponent: () =>
+      import('./features/products/products.component').then(m => m.ProductsComponent)
+  },
+  /*{
+    path: 'products/:id',
+    loadComponent: () =>
+      import('./features/products/product-details.component').then(m => m.ProductDetailsComponent)
+  },*/
+//login
   {
     path: 'login',
     loadComponent: () =>
       import('./features/auth/login/login.component').then(m => m.LoginComponent)
   },
+   {
+    path: 'orders',
+    canActivate: [authGuard],
+    loadComponent: () =>
+      import('./features/orders/orders.component').then(m => m.OrdersComponent)
+  },
+ /* {
+    path: 'account',
+    canActivate: [authGuard],
+    loadComponent: () =>
+      import('./features/account/account.component').then(m => m.AccountComponent)
+  },
+  {
+    path: 'checkout',
+    canActivate: [authGuard],
+    loadComponent: () =>
+      import('./features/checkout/checkout.component').then(m => m.CheckoutComponent)
+  },
+  {
+    path: 'favorites',
+    canActivate: [authGuard],
+    loadComponent: () =>
+      import('./features/favorites/favorites.component').then(m => m.FavoritesComponent)
+  },*/
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: '**', redirectTo: 'login' }
 ];
