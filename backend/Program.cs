8897842c4174e02fd1c83905f3d49a1de1fa815d
merimatikvesa <merima_tikvesa@hotmail.com>
2025-10-05 11:06:02 +0200

    #71 User-secret fixed

--- backend/Program.cs
+++ backend/Program.cs
@@ -3,22 +3,41 @@ using backend.Helper.Auth;
 using backend.Services.Auth;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration.UserSecrets;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using System.Reflection;
 using System.Text;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 
+var asm = Assembly.GetEntryAssembly();
+var attr = asm?.GetCustomAttribute<UserSecretsIdAttribute>();
+
+
+builder.Configuration.Sources.Clear();
+
+builder.Configuration
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true)
+    .AddJsonFile("appsettings.Local.json", optional: true, reloadOnChange: true);
+
 if (builder.Environment.IsDevelopment())
 {
-    builder.Configuration.AddUserSecrets<Program>();
-    builder.Configuration.AddJsonFile("appsettings.Local.json", optional: true, reloadOnChange: true);
+    builder.Configuration.AddUserSecrets<Program>(); // <- MORA postojati i biti u ovom projektu
 }
+
+builder.Configuration.AddEnvironmentVariables();
+
+var cfgRoot = (IConfigurationRoot)builder.Configuration;
+Console.WriteLine(">>> Config providers:");
 
 // Services
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "MobitelShop API", Version = "v1" });
@@ -48,11 +67,14 @@ builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection("Jwt"));
 builder.Services.AddScoped<IJwtService, JwtService>();
 
 
-var jwtSettings = builder.Configuration.GetSection("Jwt").Get<JwtOptions>() ?? new JwtOptions();
-if (string.IsNullOrWhiteSpace(jwtSettings.Key))
+var jwtKey = builder.Configuration["Jwt:Key"];
+if (string.IsNullOrWhiteSpace(jwtKey) || jwtKey.Length < 16)
 {
     throw new InvalidOperationException("JWT Key is missing. Set it via User Secrets: dotnet user-secrets set \"Jwt:Key\" \"<your-secret>\".");
 }
+
+var jwtSettings = builder.Configuration.GetSection("Jwt").Get<JwtOptions>() ?? new JwtOptions();
+
 
 builder.Services.AddAuthentication(options =>
 {
