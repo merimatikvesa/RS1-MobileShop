c7b80c21612c18fab1db2edb5b9fc369e42319ae
merimatikvesa <merima_tikvesa@hotmail.com>
2026-03-06 22:22:11 +0100

    Update apiBaseUrl

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://rs1-mobileshop-api.onrender.com',
+  apiBaseUrl: 'https://rs1-mobileshop-api.onrender.com/api',
   recaptchaSiteKey: '6LdBIIIsAAAAACKAD1Wa0F1wRynH-seSKu5FIj9Z'
 };
